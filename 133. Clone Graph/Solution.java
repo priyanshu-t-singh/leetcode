4b2794df71ccf601efd4f2b297ac44312b96a41d
Priyanshu T <priyanshu9943@gmail.com>
2026-08-12 10:40:47 +0530

    add solution for "133. Clone Graph" using BFS

---
+++ 133. Clone Graph/Solution.java
@@ -0,0 +1,30 @@
+// Time Complexity: O(N + M), where N is the number of nodes and M is the number of edges in the graph.
+// Space Complexity: O(N), where N is the number of nodes in the graph.
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return node;
+        
+        Queue<Node> q = new LinkedList<>();
+        Map<Node, Node> mp = new HashMap<>();
+
+        Node root = new Node(node.val);
+        mp.put(node, root);
+        q.offer(node);
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+            Node nn = mp.get(cur);
+
+            for (Node neighbor : cur.neighbors) {
+                if (!mp.containsKey(neighbor)) {
+                    q.offer(neighbor);
+                    mp.put(neighbor, new Node(neighbor.val));
+                }
+
+                nn.neighbors.add(mp.get(neighbor));
+            }
+        }
+
+        return root;
+    }
+}
