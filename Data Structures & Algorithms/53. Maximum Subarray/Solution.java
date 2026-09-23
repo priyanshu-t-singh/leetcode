246237de008df0d1bb939490fea4fb38e6b7a65f
Priyanshu T <priyanshu9943@gmail.com>
2026-09-23 13:24:14 +0530

    add solution for "53. Maximum Subarray"

---
+++ Data Structures & Algorithms/53. Maximum Subarray/Solution.java
@@ -0,0 +1,15 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE, sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (sum < 0) sum = 0;
+            sum += nums[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+}
